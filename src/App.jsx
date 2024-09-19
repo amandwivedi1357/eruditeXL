a03619edf388df20ef135e73cc4f9f64b88842bc
Aman Dwivedi <amandwivedi71124.com>
2024-09-19 13:22:10 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,29 @@ import ScrollToTop from "./routes/ScrolltoTop"
 const App = () => {
   return (
     <>
+
+<div className="whatsapp-icon-container sm:w-20 ">
+  <img 
+    src="/Home/whatsappnew.svg" 
+    alt="WhatsApp Icon" 
+    className="whatsapp-icon sm:w-16 sm:hidden shadow-black-900_b2 shadow-2xl rounded-2xl"
+  />
+  <div className="whatsapp-info cursor-pointer" onClick={()=>window.open('https://api.whatsapp.com/send?phone=919247121026','_blank')}>
+    <img 
+      src="/Home/whatsappnew.svg" 
+      alt="WhatsApp Icon" 
+      className="whatsapp-info-icon"
+    />
+    <p className="whatsapp-info-text">+91 92471 21026</p>
+  </div>
+  
+</div>
+<img 
+    src="/Home/whatsappnew.svg" 
+    alt="WhatsApp Icon" 
+    onClick={()=>window.open('https://api.whatsapp.com/send?phone=919247121026','_blank')}
+    className=" sm:w-16 sm:block hidden cursor-pointer fixed bottom-4 z-10 right-5 shadow-black-900_b2 shadow-2xl rounded-2xl"
+  />
     <SmoothScroll>
     <ScrollToTop/>
       <Allroutes/>
