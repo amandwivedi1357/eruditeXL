09f164948a9731cf266a75d803dd38f3e4e6bf51
Aman Dwivedi <amandwivedi71124.com>
2024-09-20 18:54:05 +0530

    added

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import { motion } from 'framer-motion'
 import Conc from '../../utils/conc'
 import { members } from '../../utils/data'
 import { SwipeCarousel } from './Carousel'
@@ -17,10 +18,16 @@ const About = () => {
     //         clearInterval(intervalId)
     //     })
     // }, [count]);
+    
   return (
     <div className='w-full overflow-hidden'>
                <div className='m-[40px]  flex sm:block sm:m-[20px] '>
-                <div className="flex flex-col gap-[20px] flex-1 ">
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5 }}
+                className="flex flex-col gap-[20px] flex-1 ">
                     <p className='font-inter font-bold text-[30px] sm:text-[25px]'>
                     Erudite Journey 1998
                     </p>
@@ -34,16 +41,31 @@ const About = () => {
                     Over time, the company expanded its offerings to include outbound experiential learning activities, developing approximately 60 team-building activities designed to foster deeper learning, engagement, and insights into team dynamics and behavior. 
                     </p>
 
-                </div>
-                <div className="flex-1 sm:mt-7">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5 }}
+                className="flex-1 sm:mt-7">
                     <img loading="lazy" src="/about/top.webp" alt="" className='w-full'/>
-                </div>
+                </motion.div>
                </div>
                <div className='my-[40px]  flex sm:block '>
-                <div className="flex-1 ">
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="flex-1 ">
                     <img loading="lazy" src="/about/vision.webp" alt="" className='w-full'/>
-                </div>
-                <div className="flex justify-center flex-col gap-[20px] flex-1 sm:mx-[20px]">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="flex justify-center flex-col gap-[20px] flex-1 sm:mx-[20px]">
                     <p className='font-inter font-bold text-[30px] sm:text-[25px] sm:mt-5'>
                     Vision
                     </p>
@@ -57,10 +79,15 @@ const About = () => {
                     To challenge students to step out of their comfort zones and enter the development zone, promoting personal growth and lifelong learning.
                     Our safety norms. 
                     </p>
-                </div>
+                </motion.div>
                </div>
                <div className="h-[1000px] relative sm:hidden">
-                <div className="absolute w-[351px] top-[30%] left-[5%] flex flex-col gap-4 ">
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="absolute w-[351px] top-[30%] left-[5%] flex flex-col gap-4 ">
                     <div className="w-full flex flex-col gap-2  ">
 
                             <div className="flex gap-4 items-center">
@@ -75,8 +102,13 @@ const About = () => {
                         </p>
                     </div>
                        
-                </div>
-                <div className="absolute w-[351px] top-[10%] left-[20%] flex flex-col gap-4 ">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="absolute w-[351px] top-[10%] left-[20%] flex flex-col gap-4 ">
                     <div className="w-full flex flex-col gap-2  ">
 
                             <div className="flex gap-4 items-center">
@@ -90,8 +122,13 @@ const About = () => {
                         Led by certified experiential educators ensuring engaging activities with meaningful learning outcomes.                        </p>
                     </div>
                        
-                </div>
-                <div className="absolute w-[351px] top-[25%] left-[45%] flex flex-col z-100 gap-4 ">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="absolute w-[351px] top-[25%] left-[45%] flex flex-col z-100 gap-4 ">
                     <div className="w-full flex flex-col gap-2  ">
 
                             <div className="flex gap-4 items-center">
@@ -104,8 +141,13 @@ const About = () => {
                         A focus on reflective learning helps participants absorb, understand, and apply lessons from each experience.                          </p>
                     </div>
                        
-                </div>
-                <div className="absolute w-[351px] top-[10%] left-[70%] flex flex-col gap-4 ">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="absolute w-[351px] top-[10%] left-[70%] flex flex-col gap-4 ">
                     <div className="w-full flex flex-col gap-2  ">
 
                             <div className="flex gap-4 items-center">
@@ -119,8 +161,13 @@ const About = () => {
                         Equipped with the highest quality gear, enhancing the safety and effectiveness of outdoor experiential learning.                       </p>
                     </div>
                        
-                </div>
-                <div className="absolute z-10 w-[351px] top-[37%] right-[1%] flex flex-col gap-4 ">
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+                className="absolute z-10 w-[351px] top-[37%] right-[1%] flex flex-col gap-4 ">
                     <div className="w-full flex flex-col gap-2">
 
                             <div className="flex gap-4 items-center">
@@ -134,7 +181,7 @@ const About = () => {
                         High focus on both the physical and psychological safety of participants.                        </p>
                     </div>
                        
-                </div>
+                </motion.div>
 
             <img loading="lazy" src="/Home/erudite.png" alt="" className="w-full absolute bottom-0"/>
             </div>
@@ -177,8 +224,7 @@ const About = () => {
                             </div>
                         
                         <p className="w-[85%] ml-9">
-                        With 25 years of rich experience in outbound experiential learning.
-                        </p>
+                        A focus on reflective learning helps participants absorb, understand, and apply lessons from each experience.                             </p>
                     </div>
                     <div className="w-full flex flex-col gap-2  ">
 
@@ -190,7 +236,7 @@ const About = () => {
                             </div>
                         
                         <p className="w-[85%] ml-9">
-                        With 25 years of rich experience in outbound experiential learning.                        </p>
+                        Equipped with the highest quality gear, enhancing the safety and effectiveness of outdoor experiential learning.                       </p>
                     </div>
                     <div className="w-full flex flex-col gap-2  ">
 
@@ -202,7 +248,7 @@ const About = () => {
                             </div>
                         
                         <p className="w-[85%] ml-9">
-                        With 25 years of rich experience in outbound experiential learning.
+                        High focus on both the physical and psychological safety of participants.
                         </p>
                     </div>
                        
@@ -216,7 +262,12 @@ const About = () => {
             <p className="text-center text-[30px] font-inter font-bold">
             Our Team Members                
             </p>
-            <div className='w-full grid grid-cols-4 mt-[40px]'>
+            <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
+            className='w-full grid grid-cols-4 mt-[40px]'>
                 {
                     members.map((item,idx)=>(
                         <div className='flex  flex-col gap-[15px] ' key={idx}>
@@ -226,7 +277,7 @@ const About = () => {
                         </div>
                     ))
                 }
-            </div>
+            </motion.div>
             </div>
             <div className='hidden sm:block mt-[30px]'>
                 <p className='px-[20px] text-[25px] font-bold'>Our Team Members</p>
--- src/components/Trips/index.jsx
+++ src/components/Trips/index.jsx
@@ -1,8 +1,24 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
+import { motion } from 'framer-motion';
+
+
 import Conc from '../../utils/conc'
 
 const Trips = () => {
+    
+const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.5 } }
+  };
+  
+  const slideIn = {
+    hidden: { x: -50, opacity: 0 },
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5 } }
+  };
+  const slideInRight = {
+    hidden: { x: 50, opacity: 0 },
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5 } }
+  };
     const benefits = [
         {
             head:'Cultural Immersion',
@@ -33,10 +49,25 @@ const Trips = () => {
             desc:'Staying on a houseboat on Dal Lake offers a unique blend of comfort and traditional living, enhancing the overall travel experience.'
         }
     ]
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { staggerChildren: 0.2 } }
+      };
+    
+      const itemVariants = {
+        hidden: { y: 20, opacity: 0 },
+        visible: { y: 0, opacity: 1, transition: { duration: 0.5 } }
+      };
   return (
     <div className='w-full'>
     <img loading="lazy" src='/trips/top (1).webp' alt="" className='w-full sm:w-full'/>
-    <div className="p-[40px] sm:p-[20px]">
+    <motion.div
+      variants={slideIn}
+      initial="hidden"
+      whileInView="visible"
+    //   viewport={{ once: true }}
+    //  transition={{ duration: 0.5 }}
+    className="p-[40px] sm:p-[20px]">
         <p className="text-inter font-bold text-[30px]">
         Kashmir Quest
         </p>
@@ -48,25 +79,42 @@ const Trips = () => {
           <p className="text-inter font-bold text-[20px]">Benefits of Trips</p>
           <ul className="list-none  my-4">
   {benefits.map((item, index) => (
-    <li key={index} className="mb-6">
+    <motion.li
+    variants={slideIn}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}
+    key={index} className="mb-6">
       <h3 className="font-semibold my-2">{item.head}</h3>
       <p>{item.desc}</p>
-    </li>
+    </motion.li>
   ))}
 </ul>
         </div>
 
-   </div>
+   </motion.div>
    <p className='font-inter font-bold text-[30px] p-[40px] sm:p-[20px] sm:mt-[-40px]'>
         The Itinerary
     </p>
    <div className=''>
    
     <div className='w-full h-[115rem] sm:h-[180rem] mt-[30px] sm:mt-0 relative'>
-        <img loading="lazy" src="/trips/day1.webp" alt="" className='w-[40%] sm:w-full absolute right-0'/>
-        <div className="w-[22%]  sm:w-full absolute top-[5%] right-[28%] sm:left-2 sm:top-[12%] flex flex-col gap-2  ">
+        <motion.img
+         variants={slideInRight}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+        loading="lazy" src="/trips/day1.webp" alt="" className='w-[40%] sm:w-full absolute right-0'/>
+        <motion.div
+          variants={fadeIn}
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+        className="w-[22%]  sm:w-full absolute top-[5%] right-[28%] sm:left-2 sm:top-[12%] flex flex-col gap-2  ">
 
-                        <div className='py-4 text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
+                        <div 
+                       
+                        className='py-4 text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
                             Day-1
                         </div>
 
@@ -79,8 +127,13 @@ const Trips = () => {
                         
                         <p className="w-[85%] ml-9 sm:ml-9">
                         Enjoy the Famous Shikara Ride in Dal Lake                        </p>
-                    </div>
-        <div className="w-[22%]  absolute top-[20%] sm:w-full sm:top-[28%] left-[4%] flex flex-col gap-2  ">
+                    </motion.div>
+        <motion.div 
+         variants={slideIn}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+        className="w-[22%]  absolute top-[20%] sm:w-full sm:top-[28%] left-[4%] flex flex-col gap-2  ">
 
                         <div className='py-4 text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
                             Day-2
@@ -95,14 +148,29 @@ const Trips = () => {
                         
                         <p className="w-[85%] ml-9 sm:ml-9">
                         Admire the Panoramic Views from Asia's Highest Cable Car                    </p>
-                    </div>
+                    </motion.div>
                     <img loading="lazy" src="/trips/zipline.png" alt="" className='absolute top-[28%] left-[35%] sm:top-[18%] sm:left-2' />
                     <img loading="lazy" src="/trips/zip-1.png" alt="" className='absolute top-[28%] left-[35%] sm:hidden' />
                     <img loading="lazy" src="/trips/zip-2.png" alt="" className='absolute top-[29.5%] right-[35%] sm:left-2 sm:top-[18%]' />
                     <img loading="lazy" src="/trips/zip-3.png" alt="" className='absolute top-[31%] right-[2%] sm:right-2 sm:top-[19%] sm:w-[40%]' />
-                    <img loading="lazy" src="/trips/day3.webp" alt="" className='absolute top-[38%] left-[0] sm:w-full sm:top-[33%]  ' />
-                    <img loading="lazy" src="/trips/day5.webp" alt="" className='absolute bottom-[12%] left-[0] sm:w-full sm:bottom-[36%]' />
-                    <img loading="lazy" src="/trips/day6.webp" alt="" className='absolute bottom-[0%] right-[0] sm:w-full sm:bottom-[15%]' />
+                    < motion.img
+                     variants={slideIn}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+                    loading="lazy" src="/trips/day3.webp" alt="" className='absolute top-[38%] left-[0] sm:w-full sm:top-[33%]  ' />
+                    < motion.img
+                     variants={slideIn}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+                    loading="lazy" src="/trips/day5.webp" alt="" className='absolute bottom-[12%] left-[0] sm:w-full sm:bottom-[36%]' />
+                    < motion.img
+                     variants={slideIn}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+                    loading="lazy" src="/trips/day6.webp" alt="" className='absolute bottom-[0%] right-[0] sm:w-full sm:bottom-[15%]' />
                     <div className="w-[22%] sm:top-[45%] sm:left-4 absolute sm:w-full top-[40%] left-[34%] flex flex-col gap-2  ">
 
 <div className='py-4   text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
@@ -128,14 +196,14 @@ Day 4
     <div className="flex gap-4 items-center">
     <Conc/>
 <p className="font-bold font-inter text-20px">
-Visit to Saffron Fields and Houseboat Making Unit
-</p>
+Enchanting Sonamarg A Journey to Alpine Serenity</p>
     </div>
 
 <p className="w-[85%] ml-9 sm:ml-9">
-Learn about saffron cultivation and its significance, and understand the intricate process of houseboat construction.                       </p>
+Enjoy a fun horse ride through picturesque landscapes to Thajiwas Glacier's viewpoint. You can also experience the adventurous white water rafting at the Sindh River.
+                       </p>
 </div>
-<div className="w-[22%] sm:w-full sm:left-4 sm:bottom-[7%]  absolute bottom-[25%] right-[34%] flex flex-col gap-2  ">
+<div className="w-[22%]  sm:w-full sm:left-4 sm:bottom-[7%]  absolute bottom-[25%] right-[34%] flex flex-col gap-2  ">
 
 <div className='py-4 text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
 Day 5
@@ -144,15 +212,15 @@ Day 5
     <div className="flex gap-4 items-center">
     <Conc/>
 <p className="font-bold font-inter text-20px">
-Visit to Saffron Fields and Houseboat Making Unit
+Pahalgam: A picture-perfect escape
 </p>
     </div>
 
 <p className="w-[85%] ml-9 sm:ml-9">
-Learn about saffron cultivation and its significance, and understand the intricate process of houseboat construction.                       </p>
+Enjoy the lush greenery, rivers, and mountains, and explore local villages to interact with residents.                       </p>
 </div>
 
-<div className="w-[22%] sm:w-full sm:left-4 sm:bottom-[0%]  absolute bottom-[25%] right-[34%] flex flex-col gap-2  ">
+<div className="w-[22%] sm:w-full sm:left-4 sm:bottom-[0%]  absolute bottom-[2%] right-[37%] flex flex-col gap-2  ">
 
 <div className='py-4 text-white bg-[#DE8500] w-[7rem] rounded-md text-center'>
 Day 6
@@ -161,12 +229,12 @@ Day 6
     <div className="flex gap-4 items-center">
     <Conc/>
 <p className="font-bold font-inter text-20px ">
-Visit to Saffron Fields and Houseboat Making Unit
+Cultural and Recreational Activities
 </p>
     </div>
 
 <p className="w-[85%] ml-9 sm:ml-9 ">
-Learn about saffron cultivation and its significance, and understand the intricate process of houseboat construction.                       </p>
+Try traditional Kashmiri food and visit local markets, temples, and gardens.                       </p>
 </div>
     </div>
    </div>
